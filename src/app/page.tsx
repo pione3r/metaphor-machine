1424e1b17f01a093375b2ae1bdb46a9bcd904535
pione3r <dev2diver@gmail.com>
2023-08-26 22:41:36 +0900

    ✨ Feat: 은 / 는, 이 / 가, 을 / 를 구별하여 문장 생성하는 기능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,6 @@
 
 import { useState } from 'react';
 
-const adjectives = [
-	'환상적인',
-	'아름다운',
-	'야생의',
-	'신비로운',
-	'강력한',
-	'창의적인',
-	'우아한',
-	'화려한',
-	'놀라운',
-	'영감을 주는',
-	'흥미로운',
-	'유쾌한',
-	'따뜻한',
-	'고요한',
-	'열정적인',
-	'유능한',
-	'낭만적인',
-	'용감한',
-	'낙천적인',
-	'유연한',
-	'달콤한',
-	'활기찬',
-	'귀여운',
-	'정열적인',
-	'고귀한',
-	'모험적인',
-	'담담한',
-	'친절한',
-	'소박한',
-	'빈틈없는',
-];
-
 const nouns = [
 	'꽃',
 	'산',
@@ -66,14 +33,148 @@ const nouns = [
 	'일몰',
 	'반딧불이',
 	'도시',
+	'나',
+	'너',
+	'귀뚜라미',
+	'코스모스',
+	'집',
+	'밤',
+	'눈',
+	'사람',
+	'마음',
+	'거리',
+	'얼굴',
+	'소리',
+	'아침',
+	'가을',
+	'생각',
+	'불',
+	'어둠',
+	'달',
+	'봄',
+	'아이',
+	'방',
+	'노래',
+	'나무',
+	'손',
+	'그림자',
+	'길',
+	'눈물',
+	'돌',
+	'기차',
+	'끝',
+	'언덕',
+	'저녁',
+	'어머니',
+	'머리',
+	'물결',
+	'겨울',
+	'코',
+	'조개',
+	'포기',
+	'햇님',
+	'햇빛',
+	'황혼',
+	'단풍',
+	'강물',
+	'편지',
+	'비',
+	'고향',
+	'닭',
+	'꽃밭',
+	'구름',
+	'잎',
+	'마을',
+	'지붕',
+	'나비',
+	'발',
+	'전차',
+	'성벽',
+	'해바라기',
+	'시계',
+	'전등',
+	'글씨',
+	'울음',
+	'뼈',
+	'젊은이',
+	'남대문',
+	'간판',
+	'세계',
+	'창',
+	'논',
+	'여름',
+	'공부',
+	'안개',
+	'바구니',
+	'혼자',
+	'땅',
+	'춤',
+	'터널',
+	'지도',
+	'자국',
+	'처음',
+	'감자',
+	'시대',
+	'우주',
+	'계절',
+	'달걀',
+	'연기',
+	'굴뚝',
+	'종이',
+	'죽음',
+	'물',
+	'물감',
+	'처마',
+	'병아리',
+	'달빛',
+	'돌담',
+	'걱정',
+	'비둘기',
+	'공기',
+	'민들레',
+	'살구나무',
+	'십자가',
+	'흙',
+	'손가락',
+	'뒷골목',
+	'진달래',
+	'문살',
+	'바닷가',
+	'바위',
+	'슬픔',
+	'번개',
+	'잔디밭',
+	'나뭇가지',
+	'노트',
 ];
+
+const particlesForLastConsonant = ['은', '이', '을'];
+
+const particlesForNoLastConsonant = ['는', '가', '를'];
 
 const getRandomIndex = (array: string[]) => Math.floor(Math.random() * array.length);
 
-const getRandomMetaphor = () => `${adjectives[getRandomIndex(adjectives)]} ${nouns[getRandomIndex(nouns)]}`;
+const getRandomParticle = (str: string) => {
+	// 초성 인덱스 = ((한글 유니코드값 - 0xAC00) / 28) / 21
+	// 중성 인덱스 = ((한글 유니코드값 - 0xAC00) / 28) % 21
+	// 종성 인덱스 = (한글 유니코드값 - 0xAC00) % 28
+	const lastCharUnicode = str.charCodeAt(str.length - 1);
+	const isHasLastConsonant = (lastCharUnicode - 0xac00) % 28;
+	return isHasLastConsonant
+		? particlesForLastConsonant[getRandomIndex(particlesForLastConsonant)]
+		: particlesForNoLastConsonant[getRandomIndex(particlesForNoLastConsonant)];
+};
+
+const getRandomMetaphor = () => {
+	const frontWord = nouns[getRandomIndex(nouns)];
+	const middleWord = nouns[getRandomIndex(nouns)];
+	const backWord = nouns[getRandomIndex(nouns)];
+
+	return `${frontWord}${getRandomParticle(frontWord)} ${middleWord}의 ${backWord}`;
+};
 
 export default function Home() {
-	const [metaphor, setMetaphor] = useState(getRandomMetaphor());
+	const [metaphor, setMetaphor] = useState('은유 기계');
 
 	return (
 		<div>
