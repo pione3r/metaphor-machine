cae9ea9632e824305657f300b5961302f3aeceb5
pione3r <dev2diver@gmail.com>
2023-08-28 19:57:04 +0900

    🚚 Chore: 메타데이터 추가 / next-sitemap init

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: 'https://metaphor-machine.vercel.app/',
+	generateRobotsTxt: true,
+	changefreq: 'daily',
+	priority: 1,
+	sitemapSize: 7000,
+	generateRobotsTxt: true,
+	exclude: [],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,19 @@ import ReactQueryProvider from '@/lib/ReactQueryProvider';
 import RecoilProvider from '@/lib/RecoilProvider';
 import StyledComponentsRegistry from '@/lib/StyledComponentsRegistry';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Metaphor Machine',
+	description: 'This is Metaphor Machine.. R.I.P Eco',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html>
-			<head />
+			<head>
+				<meta name="google-site-verification" content="HWc0XbjZTgIgX4xEb3homtanP4xZlGRFA9biIl76SJA" />
+			</head>
 			<body>
 				<RecoilProvider>
 					<ReactQueryProvider>
